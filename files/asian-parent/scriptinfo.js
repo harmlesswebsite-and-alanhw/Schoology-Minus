92b0468307c6f0518baef26bd6509a7e57ffec67
weeklyd3 <timothywei.spam@gmail.com>
2022-05-26 03:22:59 +0000

    Reset i18n

--- files/asian-parent/scriptinfo.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Schoology Minus (Asian Parent Edition)
-// @description  Mod the interface but with Asian parent messages
-// @version      8.8.8.5
-// @author       weeklyd3
-// @match        https://bins.schoology.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=schoology.com
-// @grant        none
-// ==/UserScript==
--- files/typoo-land/scriptinfo.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name         Schoology Minus (Typoo Land)
-// @description  Mod the interface, but with a lot more typos than ordinary Schoology Minus
-// @version      8.8.8.5
-// @author       weeklyd3
-// @match        https://bins.schoology.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=schoology.com
-// @grant        none
-// ==/UserScript==
