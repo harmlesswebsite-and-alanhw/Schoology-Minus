5deeb66a60c10ed03addc42afcaf0c9bfb50e497
weeklyd3 <timothywei.spam@gmail.com>
2022-05-26 02:38:08 +0000

    Adding translation of blank-text. (newlang.php wizard)

---
+++ files/blank-text/scriptinfo.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Schoology Minus, but Blank
+// @description  Mod the interface, but without any messages
+// @version      8.8.8.5
+// @author       weeklyd3
+// @match        https://bins.schoology.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=schoology.com
+// @grant        none
+// ==/UserScript==
