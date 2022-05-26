c96f4a06eb2372947138796735d08e65fcf7b23a
weeklyd3 <timothywei.spam@gmail.com>
2022-05-26 20:41:53 +0000

    Adding translation of asian-parent. (newlang.php wizard)

---
+++ files/asian-parent/scriptinfo.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Schoology Minus (Asian Parent Edition)
+// @description  Mod the interface but with asian parent messages
+// @version      8.8.8.5
+// @author       weeklyd3
+// @match        https://bins.schoology.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=schoology.com
+// @grant        none
+// ==/UserScript==
